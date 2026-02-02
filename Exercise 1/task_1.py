cda753b89a2174a66a78a4a2f63de4a093464380
Julian <julian.kucia@gmail.com>
2026-02-02 23:02:52 +0100

    Initial commit

---
+++ Exercise 1/task_1.py
@@ -0,0 +1,9 @@
+# Task 1
+
+print("Hello!")
+print("What's your name?\n")
+
+# Task 2
+
+name = input("What's your name? ")
+print(name)
---
+++ Exercise 1/task_2.py
@@ -0,0 +1,11 @@
+import datetime
+
+name = input("Enter your name: ")
+year_of_birth = int(input("Enter your year of birth: "))
+
+time = datetime.datetime.now()
+current_year = time.year 
+
+age = current_year - year_of_birth
+
+print(f"{name.title()}, you are {age} years old.")
