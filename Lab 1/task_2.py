a0593e7f2907897f5e77469ab58c15c886261952
Julian <julian.kucia@gmail.com>
2026-02-03 14:08:02 +0100

    some changes

--- Lab 1/task_2.py
+++ Lab 1/task_2.py
@@ -1,6 +1,6 @@
 import datetime
 
-name = input("Enter your name: ")
+name = input("Enter your name: \n")
 year_of_birth = int(input("Enter your year of birth: "))
 
 time = datetime.datetime.now()
@@ -9,3 +9,4 @@ current_year = time.year
 age = current_year - year_of_birth
 
 print(f"{name.title()}, you are {age} years old.")
+
