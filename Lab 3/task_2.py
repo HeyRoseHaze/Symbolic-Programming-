5227eda6de5973903977dc2120b588e9e28a220d
Julian <julian.kucia@gmail.com>
2026-02-12 15:34:56 +0100

    task 2 lab 3 second try

--- Lab 3/task_2.py
+++ Lab 3/task_2.py
@@ -1,12 +1,17 @@
 import math
 
 n = int(input('Enter the number n: '))
-x = int(input('Enter the number x: '))
-N = 0
-e_list = []
-while N != n:
-    e = (x ** N) / math.factorial(N)
-    e_list.append(e)
-    N += 1
+x = float(input('Enter the number x: '))
+print()
 
-print(sum(e_list))
+e_list = []
+
+for i in range(n):
+    e = (x ** i) / math.factorial(i)
+    e_list.append(e)
+
+series_sum = sum(e_list)
+
+print(f"Sum of the series: {series_sum}")
+print(f"Math.exp({x}): {math.exp(x)}")
+
