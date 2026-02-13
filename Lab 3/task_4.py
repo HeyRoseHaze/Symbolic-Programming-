41b978fe42a7316dbb2b9180e2295a4d90ce461a
Julian <julian.kucia@gmail.com>
2026-02-13 13:17:00 +0100

    map function

--- Lab 3/task_4.py
+++ Lab 3/task_4.py
@@ -7,3 +7,9 @@ print(f'Your number consists of {len(number)} numbers.')
 total_sum = sum(int(digit) for digit in number)
 
 print(f"The sum of the digits is: {total_sum}")
+
+
+# The way with using map() function
+
+total_sum = sum(map(int, number))
+print(f"The sum of the digits is: {total_sum}")
