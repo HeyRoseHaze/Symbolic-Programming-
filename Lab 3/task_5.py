861213cfbfb262508e73a79de9d48805b42ea92f
Julian <julian.kucia@gmail.com>
2026-02-13 13:55:09 +0100

    second attempt

--- Lab 3/task_5.py
+++ Lab 3/task_5.py
@@ -5,7 +5,13 @@ random_numbers = [random.randint(0, 100) for i in range(5)]
 for index, value in enumerate(random_numbers):
     print(f"random_numbers[{index}] : {value}")
 
-print(f"\nThe list consist of {len(random_numbers)} items.")
+print(f"\nThe list consist of {len(random_numbers)} items.\n")
 
-print(f"The largest number: {max(random_numbers)}\n"
-      f"The smallest number: {min(random_numbers)}")
+max_value = max(random_numbers)
+max_index = random_numbers.index(max_value)
+
+min_value = min(random_numbers)
+min_index = random_numbers.index(min_value)
+
+print(f"The largest number: {max_value} \nIndex of largest number: {max_index}\n\n"
+      f"The smallest number: {min_value} \nIndex of smallest number: {min_index}")
