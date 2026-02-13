d9ccbbdf470a1fd26743904b0172992e371884eb
Julian <julian.kucia@gmail.com>
2026-02-13 13:13:21 +0100

    second attempt

--- Lab 3/task_4.py
+++ Lab 3/task_4.py
@@ -1,14 +1,9 @@
 
 
-number = str(input('Enter a natural number: '))
+number = (input('Enter a natural number: '))
 
-lenght = len(number)
-print(f'Your number consists of {lenght} numbers')
+print(f'Your number consists of {len(number)} numbers.')
 
-numbers = list(number)
-sum_of_numbers = []
-for i in numbers:
-    num = int(i)
-    sum_of_numbers.append(num)
+total_sum = sum(int(digit) for digit in number)
 
-print(f"The sum of the numbers of your numbers is equal to: {sum(sum_of_numbers)}")
+print(f"The sum of the digits is: {total_sum}")
